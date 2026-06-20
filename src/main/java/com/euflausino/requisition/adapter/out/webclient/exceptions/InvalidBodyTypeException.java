263759931585d271ad4b5fce32772fa3085f8453
Bruno Euflausino <brunoeuflausino@gmail.com>
2026-06-19 22:01:48 -0300

    feat: adicionando nova exception - InvalidBodyTypeException

---
+++ src/main/java/com/euflausino/requisition/adapter/out/webclient/exceptions/InvalidBodyTypeException.java
@@ -0,0 +1,7 @@
+package com.euflausino.requisition.adapter.out.webclient.exceptions;
+
+public class InvalidBodyTypeException extends RuntimeException {
+    public InvalidBodyTypeException(String message) {
+        super(message);
+    }
+}
